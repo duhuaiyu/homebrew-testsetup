0ea3c7a306faa4f504d2ea6188bed9bb8bafa5a1
Kamilla Lukaszewska <kamilla.lukaszewska@affirm.com>
2023-07-21 04:55:04 +0200

    New way

--- testsetup.rb
+++ testsetup.rb
@@ -3,7 +3,7 @@ class Testsetup < Formula
   desc "First formulea for tests"
   homepage "https://github.com/Kamilla260/testsetup"
   url "https://github.com/Kamilla260/testsetup/archive/refs/tags/v1.0.5.tar.gz"
-  sha256 "73e63f1e84b86dcd0da1064862a5e30e000a52fa4cb803e068b1ba3b1d517e88"
+  sha256 "74857911360df416cd7cdc920081c9b8c2899d09eb2a80f5011ac8666a03bf54"
 
   def install
     bin.install "testsetup"
