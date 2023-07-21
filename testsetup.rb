aad504ce4dd4050488128e73263af80e4d9ff4b1
Kamilla Lukaszewska <kamilla.lukaszewska@affirm.com>
2023-07-21 09:24:29 +0200

    sum

--- testsetup.rb
+++ testsetup.rb
@@ -3,7 +3,7 @@ class Testsetup < Formula
   desc "First formulea for tests"
   homepage "https://github.com/Kamilla260/testsetup"
   url "https://github.com/Kamilla260/testsetup/archive/refs/tags/v1.0.6.tar.gz"
-  sha256 "74857911360df416cd7cdc920081c9b8c2899d09eb2a80f5011ac8666a03bf54"
+  sha256 "92546151bbdff153d080a08b99da800978b2283cf4beee272ff50684c3c41d26"
 
   def install
     bin.install "testsetup"
