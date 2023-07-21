98cd3e448edeb995cb5eff94e30d953ac7f18313
Kamilla Lukaszewska <kamilla.lukaszewska@affirm.com>
2023-07-21 04:54:25 +0200

    New way

--- testsetup.rb
+++ testsetup.rb
@@ -2,7 +2,7 @@ require_relative "lib/private_strategy"
 class Testsetup < Formula
   desc "First formulea for tests"
   homepage "https://github.com/Kamilla260/testsetup"
-  url "https://github.com/Kamilla260/testsetup/archive/refs/tags/v1.0.3.tar.gz"
+  url "https://github.com/Kamilla260/testsetup/archive/refs/tags/v1.0.5.tar.gz"
   sha256 "73e63f1e84b86dcd0da1064862a5e30e000a52fa4cb803e068b1ba3b1d517e88"
 
   def install
