4ba50af9ff01abd388acf0846c7fd107c4b3b29c
Kamilla Lukaszewska <kamilla.lukaszewska@affirm.com>
2023-07-23 23:43:46 +0200

    frogg test

--- testsetup.rb
+++ testsetup.rb
@@ -1,8 +1,8 @@
 require_relative "lib/private_strategy"
 class Testsetup < Formula
   desc "First formulea for tests"
-  homepage "https://github.com/Kamilla260/testsetup"
-  url "https://github.com/Kamilla260/testsetup/archive/refs/tags/v1.0.7.tar.gz"
+  homepage "https://froggytesto.jfrog.io/"
+  url "https://froggytesto.jfrog.io/artifactory/testsetup/"
   sha256 "60a806941f6592c4b65b0e541818b8e01888d4bb9fbeb20ea28d978101d01eb7"
 
   def install
