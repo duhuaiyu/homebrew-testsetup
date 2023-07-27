e7c8cb4d8e68600e1dbc2ef37a55e3e302c25166
Kamilla Lukaszewska <kamilla.lukaszewska@affirm.com>
2023-07-27 15:40:55 +0200

    version

--- testsetup.rb
+++ testsetup.rb
@@ -1,8 +1,7 @@
-# require_relative "lib/private_strategy"
 class Testsetup < Formula
   desc "First formulea for tests"
   homepage "https://froggytesto.jfrog.io/"
-  url "https://froggytesto.jfrog.io/artifactory/testsetup/testsetup-1.0.8.tar.gz"
+  url "https://froggytesto.jfrog.io/artifactory/testsetup/testsetup-1.0.9.tar.gz"
   sha256 "a91cee26bcc09c4676ccfbbab235c11bf6d1a085ca59d5c074d085e60b3488de"
 
   def install
