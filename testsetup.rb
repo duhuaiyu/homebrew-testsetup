6c1e6a9f0cc1833de871eb19fdfc3dcccf737925
Kamilla Lukaszewska <kamilla.lukaszewska@affirm.com>
2023-08-03 00:16:20 +0200

    v

--- testsetup.rb
+++ testsetup.rb
@@ -2,7 +2,7 @@ class Testsetup < Formula
   include Language::Python::Virtualenv
   desc "First formulea for tests"
   homepage "https://froggytesto.jfrog.io/"
-  url "https://froggytesto.jfrog.io/artifactory/testsetup/testsetup-1.0.9.tar.gz"
+  url "https://froggytesto.jfrog.io/artifactory/testsetup/testsetup-1.1.0.tar.gz"
   sha256 "4fc1db5d8c9bdd5993c430c66eae9a0b3f5745cd4e73208705981598d7494769"
 
   depends_on "python@3.8"
