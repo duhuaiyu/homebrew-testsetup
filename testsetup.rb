f6783ea63b7e81879490c469fa0ef7ae5869a1bb
Kamilla Lukaszewska <kamilla.lukaszewska@affirm.com>
2023-08-03 00:03:59 +0200

    pack

--- testsetup.rb
+++ testsetup.rb
@@ -5,7 +5,7 @@ class Testsetup < Formula
   url "https://froggytesto.jfrog.io/artifactory/testsetup/testsetup-1.0.9.tar.gz"
   sha256 "4fc1db5d8c9bdd5993c430c66eae9a0b3f5745cd4e73208705981598d7494769"
 
-  depends_on "python@3.10"
+  depends_on "python@3.8"
 
   resource "httptools" do
     url "https://files.pythonhosted.org/packages/d9/6f/aad92c3f090e2f74dd728d58d3bba4c832d35199814af99673ee7300b582/httptools-0.1.1.tar.gz"
