9b6926a6e868bcc23273a9c918327ee0c92a859d
Kamilla Lukaszewska <kamilla.lukaszewska@affirm.com>
2023-08-03 00:58:42 +0200

    v

--- testsetup.rb
+++ testsetup.rb
@@ -3,7 +3,7 @@ class Testsetup < Formula
   desc "First formulea for tests"
   homepage "https://froggytesto.jfrog.io/"
   url "https://froggytesto.jfrog.io/artifactory/testsetup/testsetup-1.1.1.tar.gz"
-  sha256 "f4fd37339acdcf5fb153d7411bf627682689d04917da7db90443202003be4440"
+  sha256 "6177230693948afbff0e4226c0cdba6bdf34839b60a86c7c9bfb46ca8ebdd350"
 
   depends_on "python@3.8"
 
