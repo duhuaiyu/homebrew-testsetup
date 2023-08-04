695ed7fbafb9a3ab5991ef4a8df6cc70a90ebd84
Huaiyu Du <huaiyu.du@affirm.com>
2023-08-04 12:17:59 +0200

    abc

--- Formula/testsetup.rb
+++ Formula/testsetup.rb
@@ -5,7 +5,10 @@ class Testsetup < Formula
   url "https://files.pythonhosted.org/packages/f2/ba/1b5f182c3f1769c0863bcaa77406bdcb81c92e31bb579959c01b1d8951c0/datasette-0.50.2.tar.gz"
   sha256 "72e3127a5007103e2b2e7e35172d7da256471c54370447199ffafb631526c0b4"
 
-  depends_on "python@3.8"
+  depends_on "python-typing-extensions"
+  depends_on "python@3.11"
+  depends_on "pyyaml"
+  depends_on "six"
 
   resource "httptools" do
     url "https://files.pythonhosted.org/packages/d9/6f/aad92c3f090e2f74dd728d58d3bba4c832d35199814af99673ee7300b582/httptools-0.1.1.tar.gz"
