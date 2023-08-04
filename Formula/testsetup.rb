baa3bba096be5b09a0201a83909742b86d54c618
Huaiyu Du <huaiyu.du@affirm.com>
2023-08-04 12:21:58 +0200

    abc

--- Formula/testsetup.rb
+++ Formula/testsetup.rb
@@ -10,10 +10,11 @@ class Testsetup < Formula
   depends_on "pyyaml"
   depends_on "six"
 
-  resource "httptools" do
-    url "https://files.pythonhosted.org/packages/d9/6f/aad92c3f090e2f74dd728d58d3bba4c832d35199814af99673ee7300b582/httptools-0.1.1.tar.gz"
-    sha256 "41b573cf33f64a8f8f3400d0a7faf48e1888582b6f6e02b82b9bd4f0bf7497ce"
+  resource "anyio" do
+    url "https://files.pythonhosted.org/packages/28/99/2dfd53fd55ce9838e6ff2d4dac20ce58263798bd1a0dbe18b3a9af3fcfce/anyio-3.7.1.tar.gz"
+    sha256 "44a3c9aba0f5defa43261a8b3efb97891f2bd7d804e0e1f56419befa1adfc780"
   end
+
 
   def install
     virtualenv_install_with_resources
